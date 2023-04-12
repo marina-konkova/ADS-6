d61b73751c75b14d74365eac4e44377052ac7188
marina-konkova <116936403+marina-konkova@users.noreply.github.com>
2023-04-12 19:51:22 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -6,62 +6,64 @@
 template<typename T, int size>
 class TPQueue {
  private:
-    T* items;
-    int max_size;
-    int head, tail;
-    int count;
- 
+  T* items;
+  int max_size;
+  int head, tail;
+  int count;
+
  public:
-    TPQueue(): max_size(size), head(0), tail(0), count(0) {
-        items = new T[max_size + 1];
-    }
-    void push(const T& item) {
-        if (count == 0) {
-            items[tail++] = item;
-            count++;
-        } else {
-            int i = tail - 1;
-            bool flag = false;
-            while (i >= head && item.prior > items[i].prior) {
-                flag = true;
-                items[i + 1] = items[i];
-                items[i] = item;
-                i--;
-            }
-            if (!flag) {
-                items[tail] = item;
-            }
-            tail++;
-            count++;
-        }
-        if (tail > max_size) {
-            tail -= max_size + 1;
-        }
-    }
-    const T& pop() {
-        count--;
-        if (head > max_size) {
-            head -= max_size + 1;
-        }
-        return items[head++];
-    }
+  TPQueue(): max_size(size), head(0), tail(0), count(0) {
+    items = new T[max_size + 1];
+  }
 
-    T& front() {
-        assert(count > 0);
-        return items[head];
+  void push(const T& item) {
+    if (count == 0) {
+      items[tail++] = item;
+      count++;
+    } else {
+      int i = tail - 1;
+      bool flag = false;
+      while (i >= head && item.prior > items[i].prior) {
+        flag = true;
+        items[i + 1] = items[i];
+        items[i] = item;
+        i--;
+      }
+      if (!flag) {
+        items[tail] = item;
+      }
+      tail++;
+      count++;
     }
+    if (tail > max_size) {
+      tail -= max_size + 1;
+    }
+  }
 
-    bool isEmpty() const {
-        return count == 0;
+  const T& pop() {
+    count--;
+    if (head > max_size) {
+      head -= max_size + 1;
     }
+    return items[head++];
+  }
 
-    bool isFull() const {
-        return count == max_size;
-    }
+  T& front() {
+    assert(count > 0);
+    return items[head];
+  }
 
-    ~TPQueue() {
-        delete[] items;
-    }
+  bool isEmpty() const {
+    return count == 0;
+  }
+
+  bool isFull() const {
+    return count == max_size;
+  }
+
+  ~TPQueue() {
+    delete[] items;
+  }
 };
 
 struct SYM {

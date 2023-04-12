9c1f1601ea9ffe3c97d59134b5bfa227476c2347
marina-konkova <116936403+marina-konkova@users.noreply.github.com>
2023-04-12 19:32:33 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,12 +4,68 @@
 
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+private:
+    T* items;
+    int max_size;
+    int head, tail;
+    int count;
+public:
+    TPQueue(): max_size(size), head(0), tail(0), count(0) {
+        items = new T[max_size + 1];
+    }
+    void push(const T& item) {
+        if (count == 0) {
+            items[tail++] = item;
+            count++;
+        } else {
+            int i = tail - 1;
+            bool flag = false;
+            while (i >= head && item.priority > items[i].priority) {
+                flag = true;
+                items[i + 1] = items[i];
+                items[i] = item;
+                i--;
+            }
+            if (!flag) {
+                items[tail] = item;
+            }
+            tail++;
+            count++;
+        }
+        if (tail > max_size) {
+            tail -= max_size + 1;
+        }
+    }
+    const T& pop() {
+        count--;
+        if (head > max_size) {
+            head -= max_size + 1;
+        }
+        return items[head++];
+    }
+
+    T& front() {
+        assert(count > 0);
+        return items[head];
+    }
+
+    bool isEmpty() const {
+        return count == 0;
+    }
+
+    bool isFull() const {
+        return count == max_size;
+    }
+
+    ~TPQueue() {
+        delete[] items;
+    }
 };
 
 struct SYM {
-  char ch;
-  int prior;
+    char ch;
+    int prior;
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
+
